330b2d861c261b04c89571c7090a1893c9a8df1d
Sayan Chimitov <66716864+SayanMountain@users.noreply.github.com>
2021-07-29 19:08:20 +0300

    hm v1

---
+++ src/test/java/HomeWorkApp.java
@@ -0,0 +1,20 @@
+public class HomeWorkApp {
+    public static void main (String [] argc) {
+        PrintThreeWords temp = new PrintThreeWords();
+        temp.print_three_names ();
+
+        CheckSumSign temp4 = new CheckSumSign();
+        temp4.check_neg_or_pos ();
+
+        PrintColor temp2 = new PrintColor();
+        temp2.check_color ();
+
+        CompareNumbers temp3 = new CompareNumbers();
+        temp3.check_num ();
+    }
+}
+
+
+//**   new - выделение памяти под новый объект
+//**  temp - название объекта конкретного класса
+//**     . - обращение к методу
